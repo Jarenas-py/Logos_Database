2d1c6fe9d4e485f623691e90e4afa8c4a7f39761
Jarenas-py <professionaljarenas@gmail.com>
2025-09-02 21:14:42 +0800

    topic 3 complete

--- University_Files/3rd_Year_1st_Sem/CPET11-L/activities/activity2/topic3/topic3/topic3.ino
+++ University_Files/3rd_Year_1st_Sem/CPET11-L/activities/activity2/topic3/topic3/topic3.ino
@@ -1,61 +1,53 @@
-// Pin mapping for the two digits
-int leds_Uno[] = {2, 3, 4, 5, 6, 7, 8};     // First digit (units)
-int leds_Dos[] = {22, 23, 24, 25, 26, 27, 28}; // Second digit (tens)
-
-int button_Uno = 9;   // decrement button
-int button_Dos = 10;  // increment button
-
-int number = 0; // 0–99 counter
-
-// Digit patterns for common cathode 7-seg display (a–g)
-// 1 = segment ON, 0 = OFF
-int digits[10][7] = {
-  {1,1,1,1,1,1,0}, // 0
-  {0,1,1,0,0,0,0}, // 1
-  {1,1,0,1,1,0,1}, // 2
-  {1,1,1,1,0,0,1}, // 3
-  {0,1,1,0,0,1,1}, // 4
-  {1,0,1,1,0,1,1}, // 5
-  {1,0,1,1,1,1,1}, // 6
-  {1,1,1,0,0,0,0}, // 7
-  {1,1,1,1,1,1,1}, // 8
-  {1,1,1,1,0,1,1}  // 9
+int leds_Uno[] = {2, 3, 4, 5, 6, 7, 8};
+int leds_Dos[] = {22, 23, 24, 25, 26, 27, 28};
+int button_Uno = 9;
+int button_Dos = 10;
+int numbers = 0;
+int digit_Array[10][7] = {
+  {1, 1, 1, 1, 1, 1, 0},
+  {0, 1, 1, 0, 0, 0, 0},
+  {1, 1, 0, 1, 1, 0, 1},
+  {1, 1, 1, 1, 0, 0, 1},
+  {0, 1, 1, 0, 0, 1, 1},
+  {1, 0, 1, 1, 0, 1, 1},
+  {1, 0, 1, 1, 1, 1, 1},
+  {1, 1, 1, 0, 0, 0, 0},
+  {1, 1, 1, 1, 1, 1, 1},
+  {1, 1, 1, 1, 0, 1, 1},
 };
 
-void setup() {
-  for (int i = 0; i < 7; i++) {
+void setup(){
+  for(int i = 0 ; i < 7 ; i++){
     pinMode(leds_Uno[i], OUTPUT);
     pinMode(leds_Dos[i], OUTPUT);
   }
 
-  pinMode(button_Uno, INPUT_PULLUP); // use pull-ups
-  pinMode(button_Dos, INPUT_PULLUP);
+  pinMode(button_Uno, INPUT);
+  pinMode(button_Dos, INPUT);
 }
 
-void displayDigit(int pins[], int digit) {
-  for (int i = 0; i < 7; i++) {
-    digitalWrite(pins[i], digits[digit][i]);
-  } 
+void segmentOutput(int pins[], int digit){
+  for(int i = 0 ; i < 7 ; i++){
+    digitalWrite(pins[i], digit_Array[digit][i]);
+}
 }
 
-void loop() {
-  // Button Uno: decrement
-  if (digitalRead(button_Uno) == LOW) {  // active low with INPUT_PULLUP
-    number--;
-    if (number < 0) number = 99;
-    delay(250); // debounce
+void loop(){
+  if (digitalRead(button_Uno) == HIGH){
+    numbers--;
+    if (numbers < 0) numbers = 99;
+    delay(150);
   }
-
-  // Button Dos: increment
-  if (digitalRead(button_Dos) == LOW) {
-    number++;
-    if (number > 99) number = 0;
-    delay(250); // debounce
+  
+  if (digitalRead(button_Dos) == HIGH){
+    numbers++;
+    if (numbers > 99) numbers = 0;
+    delay(150);
   }
+  
+  int tens = numbers / 10;
+  int ones = numbers % 10;
 
-  int tens = number / 10;
-  int ones = number % 10;
-
-  displayDigit(leds_Uno, ones);
-  displayDigit(leds_Dos, tens);
+  segmentOutput(leds_Uno, tens);
+  segmentOutput(leds_Dos, ones);
 }
